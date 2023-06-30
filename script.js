8c9889ae1bc0d4b2b53d5d4f9786e6bf1015dc28
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-30 12:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js
+const element = document.getElementById("level");
+const domLevel = getDOMLevel(element);
+alert(`The level of the element is: ${domLevel}`);
+
+function getDOMLevel(element) {
+  let level = 0;
+  let currentElement = element;
+
+  while (currentElement.parentNode !== document.body) {
+    level++;
+    currentElement = currentElement.parentNode;
+  }
+
+  return level;
+}
